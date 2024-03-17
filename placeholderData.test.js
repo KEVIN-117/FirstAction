ec8c722e293024b94c16523731aa7888e54853e4
Kevin Alexis Rodriguez Condori <sis414.kevinrodriguez17@gmail.com>
2024-03-17 16:52:47 -0400

    js app

--- placeholderData.test.js
+++ placeholderData.test.js
@@ -1,4 +1,4 @@
-const placeholderData = require('../placeholder-data')
+const placeholderData = require('./placeholder-data')
 
 test('placeholderData has 4 items', () => {
     expect(placeholderData)
--- src/index.js
+++
@@ -1,7 +0,0 @@
-const {placeholderData} = require('../placeholder-data')
-
-placeholderData.forEach(item => {
-    console.log(`Title: ${item.title}`)
-    console.log(`Content: ${item.content}`)
-    console.log('---')
-})
